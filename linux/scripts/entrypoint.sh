50c23c20d47c6d664eac027baa8976dc8237e1f8
Ableytner <emanuel.ableitner@gmx.at>
2024-10-08 21:19:15 +0200

    fix cleanup on exit

--- linux/scripts/entrypoint.sh
+++ linux/scripts/entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-/setup.sh
+# [SETUP]
 
-/start.sh
+# start docker
+echo "Starting docker..."
+sudo service docker start
+
+# [START]
+
+RUNNER_NAME=$RUNNER_NAME
+REPO=$REPO
+ACCESS_TOKEN=$TOKEN
+
+cd /home/runner/actions-runner
+
+REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+
+cleanup() {
+    echo "Removing runner..."
+    # token is only valid for 1h, so it needs to be re-queried
+    # https://github.com/actions/runner/discussions/1799#discussioncomment-2747605
+    REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh remove --token ${REG_TOKEN}
+}
+
+trap cleanup SIGINT SIGTERM
+
+./run.sh & wait $!
--- linux/scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# start docker
-echo "Starting docker..."
-sudo service docker start
