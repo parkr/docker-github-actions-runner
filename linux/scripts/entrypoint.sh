ccc1585e85b9a079111f6b4ef837fc177de9bc6c
Ableytner <emanuel.ableitner@gmx.at>
2025-08-29 21:37:56 +0200

    correct if syntax

--- linux/scripts/entrypoint.sh
+++ linux/scripts/entrypoint.sh
@@ -23,7 +23,7 @@ REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 USER=$(echo $REPO | cut -d '/' -f 1)
 
-if [[ $(curl -X GET -H "Accept: application/vnd.github+json" https://api.github.com/users/${USER} | jq .type --raw-output ) == "User" ]]
+if [[ $(curl -X GET -H "Accept: application/vnd.github+json" https://api.github.com/users/${USER} | jq .type --raw-output ) == "User" ]]; then
     echo "Repository is owned by an user"
     REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 else
